1c2b4b4d6301f192dd53a9cdeb0ec45e75c3335f
Jassem Manita <jasemmanita00@gmail.com>
2023-12-04 15:57:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var subjectsData = {
-    MP: ['Algebre', 'Analyse', 'Physique', 'Chimie', 'MSI', 'Informatique', 'Francais', 'Anglais'],
-    PC: ['Algebre', 'Analyse', 'Physique', 'Chimie', 'MSI', 'Informatique', 'Francais', 'Anglais'],
-    PT: ['Algebre', 'Analyse', 'Physique', 'Chimie', 'MSI', 'Informatique', 'Francais', 'Anglais']
+    MP: ['Algebre', 'Analyse', 'Physique', 'Chimie', 'Informatique', 'Sti', 'Francais', 'Anglais'],
+    PC: ['Algebre', 'Analyse', 'Physique', 'Chimie', 'Informatique', 'Sti', 'Francais', 'Anglais'],
+    PT: ['Algebre', 'Analyse', 'Physique', 'Chimie', 'Informatique', 'Sti', 'Francais', 'Anglais']
 };
 
 function updateSubjects() {
@@ -12,8 +12,15 @@ function updateSubjects() {
 
     subjectsData[major].forEach(function (subject, index) {
         var subjectDiv = document.createElement('div');
-        subjectDiv.innerHTML = '<label for="subject' + index + '">' + subject + ' :</label>' +
-            '<input type="text" id="subject' + index + '_y' + year + '" name="subject' + index + '" required>';
+        subjectDiv.innerHTML = `<div class="subject">
+                                    <h3>${subject}</h3>
+                                    <label for="oral${index}">Orale :</label>
+                                    <input type="number" id="oral${index}_y${year}" name="oral${index}" step="0.25" min="0" max="20" required>
+                                    <label for="ds${index}">DS :</label>
+                                    <input type="number" id="ds${index}_y${year}" name="ds${index}" step="0.25" min="0" max="20" required>
+                                    <label for="exam${index}">Exam :</label>
+                                    <input type="number" id="exam${index}_y${year}" name="exam${index}" step="0.25" min="0" max="20" required>
+                                </div>`;
         subjectsContainer.appendChild(subjectDiv);
     });
 }
@@ -32,19 +39,23 @@ function calculateGPA() {
     var year = document.getElementById('year').value;
 
     var subjects = subjectsData[major];
-    var coefficients = [7, 7, 8, 5, 5, 4, 3, 3];
+    var coefficients = [0.2, 0.3, 0.5]; // Coefficients for oral, DS, and Exam respectively
 
     var totalCredits = 0;
     var totalPoints = 0;
 
     subjects.forEach(function (subject, index) {
-        var grade = parseFloat(document.getElementById('subject' + index + '_y' + year).value);
-        totalCredits += coefficients[index];
-        totalPoints += grade * coefficients[index];
+        var oral = parseFloat(document.getElementById(`oral${index}_y${year}`).value) || 0;
+        var ds = parseFloat(document.getElementById(`ds${index}_y${year}`).value) || 0;
+        var exam = parseFloat(document.getElementById(`exam${index}_y${year}`).value) || 0;
+
+        totalCredits += 1; // 1 credit per subject
+
+        totalPoints += (oral * coefficients[0] + ds * coefficients[1] + exam * coefficients[2]);
     });
 
     var semesterGPA = totalPoints / totalCredits;
 
     var resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '<p>Moyenne de Semestre pour la filière ' + major + ' en année ' + year + ' : ' + semesterGPA.toFixed(2) + '</p>';
+    resultDiv.innerHTML = `<p>Moyenne de Semestre pour la filière ${major} en année ${year} : ${semesterGPA.toFixed(2)}</p>`;
 }
